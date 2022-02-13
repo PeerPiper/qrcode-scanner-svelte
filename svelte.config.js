17814649fca08f29e1e46ac6c4959f3ae6fb20aa
Doug A <douganderson444@gmail.com>
2022-02-13 13:43:02 -0400

    configure github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
 import preprocess from 'svelte-preprocess';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +12,13 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs'
+		}),
+		paths: {
+			base: dev ? '' : '/qrcode-scanner-svelte'
+		}
 	}
 };
 
